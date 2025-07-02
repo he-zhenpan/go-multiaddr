9bd18115682cd8020d1fae31f55233a5336070c3
he-zhenpan <he.zhenpan@aldelo.com>
2025-07-02 17:01:30 +0800

    feat: abstract network interface address retrieval with NetDriver

--- net/net.go
+++ net/net.go
@@ -11,8 +11,15 @@ import (
 	"net"
 
 	ma "github.com/multiformats/go-multiaddr"
-	anet "github.com/wlynxg/anet"
 )
+
+type NetDriver interface {
+	InterfaceAddrs() ([]net.Addr, error)
+}
+
+var inet NetDriver = getNetDriver()
+
+func SetNetInterface(n NetDriver) { inet = n }
 
 // Conn is the equivalent of a net.Conn object. It is the
 // result of calling the Dial or Listen functions in this
@@ -388,7 +395,7 @@ func WrapPacketConn(pc net.PacketConn) (PacketConn, error) {
 
 // InterfaceMultiaddrs will return the addresses matching net.InterfaceAddrs
 func InterfaceMultiaddrs() ([]ma.Multiaddr, error) {
-	addrs, err := anet.InterfaceAddrs()
+	addrs, err := inet.InterfaceAddrs()
 	if err != nil {
 		return nil, err
 	}
